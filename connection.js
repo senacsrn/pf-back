2d69b5c06313c965be42bf4ebfdddba0de009ec7
Samuel Damasceno Sales <132498977+busaikuna@users.noreply.github.com>
2024-10-08 19:50:17 -0300

    deploy

--- connection.js
+++ connection.js
@@ -1,14 +1,15 @@
 import pkg from 'pg';
-const {Pool} = pkg;
+const { Pool } = pkg;
 
-const connection = new Pool(
-    {
-        user: "postgres",
-        password: "0350365",
-        database: "patinhas_felizes",
-        host: "localhost",
-        port: "5432"
+const connection = new Pool({
+    user: "busai",
+    password: "nw2UvQaDTolAkdQSjaDzYjv9u6OpeZ2h", 
+    database: "patinhas_felizes", 
+    host: "dpg-cs2r4stsvqrc73dph73g-a.oregon-postgres.render.com", 
+    port: "5432",
+    ssl: {
+        rejectUnauthorized: false
     }
-)
+});
 
-export default connection
+export default connection;
